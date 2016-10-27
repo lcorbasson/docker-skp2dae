4a17f9f9f37760f5b0b05fce9a7163f7a4fc67be
Loïc CORBASSON <loic.corbasson@gmail.com>
2016-10-27 12:25:08 +0200

    Add usage information.

--- docker-skp2dae.sh
+++ docker-skp2dae.sh
@@ -4,8 +4,14 @@ VOL="$1"
 [ "${VOL#/}" == "$VOL" ] && VOL="$PWD/$VOL"
 if [ $# -gt 1 ]; then
 	shift
-else
+elif [ $# -eq 1 ]; then
 	[ -f "$VOL" ] && VOL="$(dirname "$VOL")"
+else
+	echo "Usage:" >&2
+	echo "	$0   FILE-AS-SEEN-FROM-HOST" >&2
+	echo "	$0   DIRECTORY-TO-SHARE-WITH-THE-CONTAINER   FILES-AS-SEEN-FROM-CONTAINER..." >&2
+	echo "Inside the container, the shared directory is /home/sketchup/filestoconvert" >&2
+	exit 1
 fi
 #docker build -t skp-converter .
 for FILE in "$@"; do
