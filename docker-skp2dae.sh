c064bb8a0b308841337642131b7d4303a6bedc73
Loïc CORBASSON <loic.corbasson@gmail.com>
2016-10-27 12:20:19 +0200

    Allow passing only a FILE parameter.

--- docker-skp2dae.sh
+++ docker-skp2dae.sh
@@ -2,7 +2,11 @@
 VOL="$1"
 [ -z "$VOL" ] && VOL="$PWD/filestoconvert"
 [ "${VOL#/}" == "$VOL" ] && VOL="$PWD/$VOL"
-shift
+if [ $# -gt 1 ]; then
+	shift
+else
+	[ -f "$VOL" ] && VOL="$(dirname "$VOL")"
+fi
 #docker build -t skp-converter .
 for FILE in "$@"; do
 	timelimit -p -T500 -t240 docker run --rm --volume "$VOL":/home/sketchup/filestoconvert --user sketchup skp-converter "$FILE"
